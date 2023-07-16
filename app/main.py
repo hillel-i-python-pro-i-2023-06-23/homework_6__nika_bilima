0f74ce21357a5a13cbd76b3ad64ab4104c4e7187
nikusion <bilima.nika2003@gmail.com>
2023-07-16 13:09:18 +0300

    Add functions from who_is_there.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from app.services.reading_the_file import reading_the_file
 from app.services.generate_users import generate_users, print_users
+from app.services.who_is_there import get_astronaut_count, print_count
 
 
 def main():
+    print("Mini-task 1:'Reading the file'")
     reading_the_file()
 
+    print("\n\nMini-task 2:'Generate_users'")
     users = generate_users(50)
     print_users(users)
+
+    print("\n\nMini-task 3:'Who is there?'")
+    count = get_astronaut_count()
+    print_count(count)
+
+    print("\n\nMini-task 4:'Average'")
