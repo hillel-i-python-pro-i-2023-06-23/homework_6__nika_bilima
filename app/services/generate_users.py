3f45165260e6975c9ee9f738a5ffc7cccb44725e
nikusion <bilima.nika2003@gmail.com>
2023-07-16 12:34:46 +0300

    Fill generate_users.py

---
+++ app/services/generate_users.py
@@ -0,0 +1,15 @@
+from faker import Faker
+
+
+def generate_users(num_of_users = 100):
+    fake = Faker()
+    for _ in range(num_of_users):
+        name = fake.first_name()
+        email = fake.email()
+
+        yield f"{name}    {email}"
+
+
+def print_users(generated_users):
+    for user in generated_users:
+        print(user)
