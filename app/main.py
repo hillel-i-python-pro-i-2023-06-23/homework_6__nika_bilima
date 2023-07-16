1cecd38451fd793e36b29474baa455259851c86f
nikusion <bilima.nika2003@gmail.com>
2023-07-16 12:39:39 +0300

    add functions from generate_users.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from app.services.reading_the_file import reading_the_file
+from app.services.generate_users import generate_users, print_users
 
 
 def main():
     reading_the_file()
+
+    users = generate_users(50)
+    print_users(users)
